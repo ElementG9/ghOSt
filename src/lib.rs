bb9d780ecae83cb52dc4c22b6613510d9aebf358
Garen Tyler <garentyler@gmail.com>
2021-02-01 08:02:51 -0700

    interrupts

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![cfg_attr(test, no_main)]
+#![feature(abi_x86_interrupt)]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
@@ -8,6 +9,7 @@ use core::panic::PanicInfo;
 
 pub mod serial;
 pub mod vga_buffer;
+pub mod interrupts;
 
 pub trait Testable {
     fn run(&self) -> ();
@@ -55,10 +57,11 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     }
 }
 
-// Entry point for `cargo xtest`
+// Entry point for `cargo test`
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
     loop {}
 }
@@ -67,4 +70,8 @@ pub extern "C" fn _start() -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     test_panic_handler(info)
+}
+
+pub fn init() {
+    interrupts::init_idt();
 }
--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,10 @@ use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
+    println!("Hello world{}", "!");
+
+    ghOSt::init();
+    x86_64::instructions::interrupts::int3();
 
     #[cfg(test)]
     test_main();
