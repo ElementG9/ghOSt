8a21b6239ddda77d4c8bbc823011376e55e05630
Garen Tyler <garentyler@gmail.com>
2021-01-31 19:19:10 -0700

    assembly bootloader hello world

--- macOS-i386-install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# A simple script for macOS to install the prerequisites.
-brew install i386-elf-gcc
-brew install i386-elf-binutils
-brew install i386-elf-gdb
--- src/klib/stdlib.c
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-int abs(int num) {
-  if (num >= 0)
-    return num;
-  else return num * -1;
-}
